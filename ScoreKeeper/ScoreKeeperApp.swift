8c8dddb7ce8c318ec0fc977f9abeca81cae63eb2
Jeremy Kim <jedo@alumni.unc.edu>
2025-05-02 00:23:46 -0400

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Jeremy Kim on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScoreKeeperTests/ScoreKeeperTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperTests.swift
+//  ScoreKeeperTests
+//
+//  Created by Jeremy Kim on 5/2/25.
+//
+
+import Testing
+@testable import ScoreKeeper
+
+struct ScoreKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
